42cb22456ad7a8d6a132429d0ce5956b8238f317
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-04-23 15:53:49 +0200

    :dizzy: :lipstick: style(firstsection): style and add effects on first section

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './style.scss';
 import menuIconAnimation from './ts/menuIconAnimation.mts';
+import LocomotiveScroll from 'locomotive-scroll';
+
+const locomotiveScroll = new LocomotiveScroll();
 
 menuIconAnimation();
